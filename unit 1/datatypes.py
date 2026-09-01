58e1ee3059270e82edb6786d14db6fdadd6b1662
JordanNacho <jbeltran889@boyslatin.net>
2026-09-01 14:24:38 +0000

    Data type notes

---
+++ unit 1/datatypes.py
@@ -0,0 +1,34 @@
+# STRING - characters that are wrapped within quotes.
+# characters can be letters, numbers, and symbols.
+# you can use both single and double quotations
+# marks for strings
+
+"jordan beltran"
+
+"123 main street"
+
+"this cost $10.00"
+
+# the hashtag is a marker that makes anything written.
+# behind it invisible to the computer
+
+# INTERGER - data type that are just whole numbers, or numbers
+# without a decimal point. we dont have to add any special symbols.
+#we can write the number as it.
+
+10
+20
+2000000
+
+# FLOATS - A number with a decimal.
+
+20.99
+3.339
+0.23
+
+# BOOLEAN - a data rype that can only be either true or false
+# we do not use quotation marks for boolean values. the computer
+# understands these words as something unique.
+
+False
+True
