88fc0e71710fb7e9dc21be3e15dd62fc8e2b805d
JordanNacho <jbeltran889@boyslatin.net>
2026-09-03 14:20:17 +0000

    I added all veriables

---
+++ unit 1/variableActivity.py
@@ -0,0 +1,43 @@
+# VARIABLES - a container that stores data. you can
+# name your conttainer almost anything you want, as long
+# as it is descripted.
+
+Name= "Jordan Beltran"
+yearsofExp= 0
+graduate= True
+
+douglas42= ''
+savage21= ''
+thomas3= ''
+j2ordan3= ''
+
+age = 10
+Age = 20
+AGE = 30
+
+print(age)
+print(Age)
+print(AGE)
+
+a= 01000
+A= 10101
+
+# when we want to create a variable, we make up a word,
+# use the eaqual sign, and then assign it as a data type.
+# whenever the computer sees our variable word, it will
+# always use the data inside.
+
+# Variable Naming Rules
+# 1. A variable MUST start with letter and cannot start with a number
+# 2. A variable can be mixed of letters and numbers But not symbols
+# 3. A variable CANNOT be a python keyword
+
+SuperMarketApple = 30
+StudentQuantity = 23
+TravelInsurance = True
+BronzeChance = 50%
+SilverChance = 40%
+goldChance = 10%
+applejuicePrice = 3.00
+orangejuicePrice = 3.50
+milkPrice = 2.50
