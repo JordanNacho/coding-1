ab5704c4497d554dc4c0a86c2d60b9c850439287
JordanNacho <jbeltran889@boyslatin.net>
2026-09-22 14:35:53 +0000

    done

---
+++ unit 2/builitinFunctions.py
@@ -0,0 +1,38 @@
+# function - simple ut; a code block of
+# instructions for computers to follow
+
+# built in function - a code block of
+# instructions for a computor to follow 
+# that was already written for us.
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+print('hello')
+year = input("what year is it?")
+print(year)
+
+# imput() a built in funcion that allows a user to
+# type in data into the terminal. the data that is passed
+# in will always be treated as a string data type
+
+# print() a built in function that allows a user to 
+# print data out in the terminal.
+
+# I went to the bathroom so I missed some code
+
+year = 1906
+print("this event took place in"+ str(year))
+
+# string concatenation - combining string data types
+# with on another. We use the plus sign (+) operator
+# to combine strings together
+
+
+# use w3school to find the definitions for each
+# datacasting function.
+# then use the data casting function in a real block of
+# code
+
+#int()- Removes all decimals and cunstucts an interger from
+# an interger literal
+
+#float()- c
